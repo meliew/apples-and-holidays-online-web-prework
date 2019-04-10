10701f15295f6f8d1983793c495a1221f1b56559
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-10 05:37:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,9 +89,12 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
+      holiday if item.include?("BBQ")
+    end
+end.flatten.compact
 end
-
-
 
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
