75c3163f0ae21aa3d28bfc75bf0c6d83b4cf1cc4
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-10 05:34:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,12 +92,13 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
       holiday if item.include?("BBQ")
-      puts holiday.to_s.split("_").collect {|item| item.capitalize!}
+    bbq = holiday.to_s.split("_").collect {|item| item.capitalize!}
+    puts "#{bbq}"
     end
   end
 end
 
-end
+
 all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
